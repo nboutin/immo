9c05eff96de03ee00083e11db252d599974dd88a
nboutin <boutwork@gmail.com>
2020-11-17 17:13:49 +0100

    Add calcul rendement brut

--- rendement.py
+++ rendement.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
+
+__DATA_FILENAME = "data.json"
+
+
 def main():
-  pass
+    
+    with open(__DATA_FILENAME, 'r') as file:
+        data = json.load(file)
+    
+    bien_immo = dict()    
+    
+    prepare_data(data, bien_immo)
+    
+    bien_immo['r_brut'] = compute_rendement_brut(data, bien_immo)
+    
+    print_repport(data, bien_immo)
+
+
+def prepare_data(data, bien_immo):
+    
+    bien_immo['loyers_sum'] = sum(data['loyers'])
+
+
+def compute_rendement_brut(data, bien_immo):
+    
+    return bien_immo['loyers_sum'] / (data['prix_achat'])
+
+
+def print_repport(data, bien_immo):
+    
+    print('Input:', data)
+    print('Output:', bien_immo)
+
 
 if __name__ == '__main__':
   main()
