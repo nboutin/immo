41ab02ae51e1045e93d93b8ac34594008aedd924
nboutin <boutwork@gmail.com>
2020-11-18 10:42:52 +0100

    Consolidate input data usage

--- rendement.py
+++ rendement.py
@@ -10,37 +10,34 @@ __DATA_FILENAME = "data.json"
 def main():
     
     with open(__DATA_FILENAME, 'r') as file:
-        data = json.load(file)
+        bien_immo = json.load(file)
     
-    bien_immo = dict()    
+    prepare_inputs(bien_immo)
     
-    prepare_data(data, bien_immo)
+    calcul_rendement_brut(bien_immo)
     
-    bien_immo['r_brut'] = calcul_rendement_brut(data, bien_immo)
-    
-    print_repport(data, bien_immo)
+    print_repport(bien_immo)
 
 
-def prepare_data(data, bien_immo):
+def prepare_inputs(bien_immo):
     
-    bien_immo['loyers_mensuel_total'] = sum(data['loyers_mensuel'])
+    bien_immo['loyers_mensuel_total'] = sum(bien_immo['loyers_mensuel'])
     bien_immo['loyers_annuel_total'] = bien_immo['loyers_mensuel_total'] * 12 
 
 
-def calcul_rendement_brut(data, bien_immo):
+def calcul_rendement_brut(bien_immo):
     
-    return calcul.rendement_brut(bien_immo['loyers_annuel_total'], data['prix_achat'])
+    bien_immo['r_brut'] = calcul.rendement_brut(bien_immo['loyers_annuel_total'], bien_immo['prix_achat'])
 
 
-def compute_rendement_net(data, bien_immo):
+def compute_rendement_net(bien_immo):
     
     return 0
 
 
-def print_repport(data, bien_immo):
+def print_repport(bien_immo):
     
-    print('Input:', data)
-    print('Output:', bien_immo)
+    print(bien_immo)
 
 
 if __name__ == '__main__':
--- test/test_rendement.py
+++ test/test_rendement.py
@@ -8,11 +8,10 @@ class TestRendement(unittest.TestCase):
 
     def testRendementBrut(self):
         
-        data = {'prix_achat' : 100000}
-        bien_immo = {'loyers_annuel_total': 500 * 12}
+        bien_immo = {'prix_achat' : 100000, 'loyers_annuel_total': 500 * 12}
         
-        r_brut = rendement.calcul_rendement_brut(data, bien_immo)
-        self.assertEqual(r_brut, 0.06)
+        rendement.calcul_rendement_brut(bien_immo)
+        self.assertEqual(bien_immo['r_brut'], 0.06)
 
 
 if __name__ == '__main__':
