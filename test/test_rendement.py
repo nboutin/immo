bf845f6e624ffd423fa1d29a7adf12c9a5b7da87
nboutin <boutwork@gmail.com>
2020-11-18 11:34:46 +0100

    Add Unittest for prepare_input

--- test/test_rendement.py
+++ test/test_rendement.py
@@ -4,6 +4,31 @@
 import unittest
 import rendement
 
+
+class TestPrepareInput(unittest.TestCase):
+
+    def testLoyerMensuelTotal_A(self):
+        
+        bien_immo = {'lots' : [{'loyer_mensuel': 100}]}
+        rendement.prepare_inputs(bien_immo)
+        
+        self.assertEqual(bien_immo['loyers_mensuel_total'], 100)
+    
+    def testLoyerMensuelTotal_B(self):
+        
+        bien_immo = {'lots' : [{'loyer_mensuel': 100}, {'loyer_mensuel': 250}]}
+        rendement.prepare_inputs(bien_immo)
+        
+        self.assertEqual(bien_immo['loyers_mensuel_total'], 350)
+        
+    def testLoyerAnnuelTotal(self):
+
+        bien_immo = {'lots' : [{'loyer_mensuel': 100}, {'loyer_mensuel': 250}]}
+        rendement.prepare_inputs(bien_immo)
+        
+        self.assertEqual(bien_immo['loyers_annuel_total'], 350*12)
+
+
 class TestRendement(unittest.TestCase):
 
     def testRendementBrut(self):
