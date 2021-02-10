ea4b067eb137bdad43138398a0738a0acd3d0d1f
nboutin <boutwork@gmail.com>
2021-02-10 15:55:49 +0100

    Add calcul taux_periodique and taux_actuariel

--- src/analyse_immo/tools.py
+++ src/analyse_immo/tools.py
@@ -4,8 +4,25 @@
 import math
 
 
-def interets_compose(capital, taux, duree_annee):
+def taux_periodique(taux_period, n_periode):
     '''
-    @return: capital cumul = capital * (1 + taux)^(duree_annee)
+    # Taux proportionnel (credit immo et pro)
+    taux_periodique = taux_period / n_periode
     '''
-    return capital * math.pow((1 + taux), duree_annee)
+    return taux_period / n_periode
+
+
+def taux_actuariel(taux_period, n_periode):
+    '''
+    # Taux actuariel (credit conso)
+    taux_periodique = (1 + taux_period)^(1/n_periode)-1
+    exemple remboursement mensuel: taux periodique = (1 + taux)^(1/12)-1
+    '''
+    return math.pow((1 + taux_period), 1 / n_periode) - 1
+
+
+def interets_compose(capital, taux_periodique, n_periode):
+    '''
+    @return: capital cumul = capital * (1 + taux_periodique)^(n_periode)
+    '''
+    return capital * math.pow((1 + taux_periodique), n_periode)
--- src/test/test_tools.py
+++ src/test/test_tools.py
@@ -3,13 +3,26 @@
 
 import unittest
 
-from analyse_immo.tools import interets_compose
+from analyse_immo.tools import interets_compose, taux_periodique, taux_actuariel
 
 
 class TestTools(unittest.TestCase):
 
+    def testTauxPeriodique(self):
+        t_an = 0.10
+        self.assertAlmostEqual(taux_periodique(t_an, 1), t_an, 4)
+        self.assertAlmostEqual(taux_periodique(t_an, 12), t_an / 12, 4)
+
+    def testtauxActuariel(self):
+        t_an = 0.10
+        self.assertAlmostEqual(taux_actuariel(t_an, 1), t_an, 2)
+        self.assertAlmostEqual(taux_actuariel(t_an, 12), 0.0080, 4)
+
     def testInteretCompose(self):
-        self.assertAlmostEqual(interets_compose(1000, 0.05, 4), 1215.51, 2)
+        t_an = 0.05
+        t_men_actu = taux_actuariel(t_an, 12)
+        self.assertAlmostEqual(interets_compose(1000, t_an, 4), 1215.51, 2)
+        self.assertAlmostEqual(interets_compose(1000, t_men_actu, 4 * 12), 1215.51, 2)
 
 
 if __name__ == '__main__':
