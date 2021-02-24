3b53b3ad55a6e720cc042571e9e383354e3c08f7
nboutin <boutwork@gmail.com>
2021-02-24 14:44:27 +0100

    Configure coverage

--- src/analyse_immo/tools/finance.py
+++ src/analyse_immo/tools/finance.py
@@ -1,5 +1,5 @@
 '''
-@author:
+@author: nboutin
 '''
 import math
 
--- src/run_coverage.sh
+++ src/run_coverage.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-#coverage run --source=analyse_immo --branch -m unittest -v test
-
-#coverage run --source=analyse_immo.bien_immo.lot --branch -m unittest -v test.bien_immo.test_lot
-#coverage run --source=analyse_immo.bien_immo.charge --branch -m unittest -v test.bien_immo.test_charge
-#coverage run --source=analyse_immo.bien_immo.bien_immo --branch -m unittest -v test.bien_immo.test_bien_immo
-coverage run --source=analyse_immo.rendement --branch -m unittest -v test.test_rendement
+coverage erase
+coverage run -a --source=analyse_immo.bien_immo.lot --branch -m unittest -v test.bien_immo.test_lot
+coverage run -a --source=analyse_immo.bien_immo.charge --branch -m unittest -v test.bien_immo.test_charge
+coverage run -a --source=analyse_immo.bien_immo.bien_immo --branch -m unittest -v test.bien_immo.test_bien_immo
+coverage run -a --source=analyse_immo.rendement --branch -m unittest -v test.test_rendement
 
 coverage report --skip-empty -m
 coverage html -d ./test/coverage/html --skip-empty
