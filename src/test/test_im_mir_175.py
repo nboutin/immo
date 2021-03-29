fe49ee35f823c20197b68ca9ffb344a1e96f2c4a
nboutin <boutwork@gmail.com>
2021-03-29 16:40:42 +0200

    Update tests

--- src/test/test_im_mir_175.py
+++ src/test/test_im_mir_175.py
@@ -22,23 +22,61 @@ class TestImMir130(unittest.TestCase):
     def testAll(self):
         analyse = Factory.make_analyse(self.input_data)
 
-        # bien_immo = analyse.bien_immo
-        # credit = analyse.credit
+        bien_immo = analyse.bien_immo
+        credit = analyse.credit
         rdt = analyse.rendement
         irpp_0 = analyse.irpp_2044_projection[0]
-        # an2044_0 = irpp_0.annexe_2044
-
-        self.assertAlmostEqual(rdt.cashflow_net_annuel(1), 2850, 0)
+        an2044_0 = irpp_0.annexe_2044
 
         impot_salaire = (((31500 + 23100) * .9) / 2.5 - 10084) * 2.5 * .11
         impot_salaire -= 143 * .66
         self.assertAlmostEqual(irpp_0.impot_sans_revenu_foncier, impot_salaire, 0)
 
-        revenu_foncier = (450 + 350 + 300) * 12 - (1000 - 3 * 100)
-        revenu_foncier *= .11
-        revenu_foncier *= .172
-        self.assertAlmostEqual(irpp_0.impots_revenu_foncier, 2937, 0)
-        self.assertAlmostEqual(irpp_0.sum_ligne(L9_impot_du), 0, 0)
+        loyer = (450 + 350 + 300) * 12
+        credit_mensualite = 9050
+        cashflow_net = loyer - credit_mensualite - 1000 - 300
+        self.assertEqual(bien_immo.loyer_nu_net_annuel(), loyer)
+        self.assertAlmostEqual(credit.get_mensualite_avec_assurance(1, 12), credit_mensualite, 0)
+        self.assertAlmostEqual(rdt.cashflow_net_annuel(1), cashflow_net, 0)
+
+        self.assertAlmostEqual(an2044_0.sum_ligne(L211_loyer_brut), loyer, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L215_total_des_recettes), loyer, 0)
+
+        charges = 1000 + 3 * 100 + 3 * 20
+        self.assertAlmostEqual(an2044_0.sum_ligne(L221_frais_administration), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L222_autre_frais_gestion), 3 * 20, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L223_prime_assurance), 3 * 100, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L224_travaux_provision), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L224_travaux_renovation), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L224_total_travaux), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L225_charges_recuperable), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L226_indemnites_eviction), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L227_taxe_fonciere), 1000, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L240_total_frais_et_charges), charges, 0)
+
+        credit_interet = 1713.6
+        credit_assurance = 532
+        credit_total = credit_interet + credit_assurance + 300
+        self.assertAlmostEqual(an2044_0.sum_ligne(L250_interet_emprunt), credit_interet, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L250_assurance_emprunteur), credit_assurance, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L250_frais_dossier), 300, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L250_frais_garantie), 0, 0)
+        self.assertAlmostEqual(an2044_0.sum_ligne(L250_total_emprunt), credit_total, 0)
+
+        revenu_foncier = loyer - charges - credit_total
+        self.assertAlmostEqual(an2044_0.sum_ligne(L420_resultat_foncier), revenu_foncier, 0)
+
+        revenu = (31500 + 23100) * .9 + revenu_foncier
+        self.assertTrue(revenu / 2.5 < 25710)
+        # impot_foncier += (min((revenu / 2.5), 25710) - 10084) * 2.5 * .11
+        # impot_foncier += (revenu / 2.5 - 25710) * 2.5 * .3
+        # impot_foncier = revenu_foncier * .172
+        # impot_foncier += (revenu / 2.5 - 10084) * 2.5 * .11 - impot_salaire
+        impot_foncier = revenu_foncier * (.11 + .172)
+
+        self.assertAlmostEqual(irpp_0.impot_sans_revenu_foncier, impot_salaire, 0)
+        self.assertAlmostEqual(irpp_0.impots_revenu_foncier, impot_foncier, 0)
+        # self.assertAlmostEqual(irpp_0.sum_ligne(L9_impot_du), impot_salaire + impot_foncier, 0)
 
         # # Bien Immo
         # total_travaux = (30 + 60 + 5 + 25 + 5 + 1.9) * 1000
