aa879d1368eadc7b7b20777437cec22045cc6b21
nboutin <boutwork@gmail.com>
2021-03-30 12:08:07 +0200

    Update openfisca revenu_foncier with decote

--- src/poc/openfisca/revenu_foncier.py
+++ src/poc/openfisca/revenu_foncier.py
@@ -4,25 +4,27 @@ import unittest
 from openfisca_france import FranceTaxBenefitSystem
 from openfisca_core.simulation_builder import SimulationBuilder
 
+annee = '2021'
 E1 = {
-    'individus': {'Nicolas': {'revenu_assimile_salaire': {'2019': 20000}},
-                  'Audrey': {'revenu_assimile_salaire': {'2019': 15000}},
-                  'Lya': {}},
-    'foyers_fiscaux': {'foyer1': {'declarants': ['Nicolas', 'Audrey'],
-                                  'personnes_a_charge': ['Lya'],
-                                  # 'f4ba': {'year:2020', 10000}
-                                  }},
-    'menages': {'roles': {'personne_de_reference': 'Nicolas',
-                          'conjoint': 'Audrey',
-                          'enfants': 'Lya'}},
-    'familles': {'roles': {'parents': ['Nicolas', 'Audrey'],
-                           'enfants': ['Lya']}},
+    'individus': {'I1': {'salaire_imposable': {annee: 20000}},
+                  'I2': {'salaire_imposable': {annee: 15000}},
+                  'I3': {}},
+    'foyers_fiscaux': {'f1': {'declarants': ['I1', 'I2'],
+                              'personnes_a_charge': ['I3']}},
+    'menages': {'roles': {'personne_de_reference': 'I1',
+                          'conjoint': 'I2',
+                          'enfants': 'I3'}},
+    'familles': {'roles': {'parents': ['I1', 'I2'],
+                           'enfants': ['I3']}},
 }
 
 E2 = {
     'individus': {'I1': {}},
     'foyers_fiscaux': {'f1': {'declarants': ['I1']}}
 }
+
+# self.sim.trace = True
+# self.sim.tracer.print_computation_log()
 
 
 class TestRevenuFoncier(unittest.TestCase):
@@ -33,12 +35,18 @@ class TestRevenuFoncier(unittest.TestCase):
         self.sim = simulation_builder.build_from_entities(tax_benefit_system, E2)
 
     def test01(self):
-        annee = '2021'
-        self.sim.set_input('revenu_assimile_salaire', annee, 30000)
-        self.assertEqual(self.sim.calculate('irpp', annee), (30000 * .9 - 10084) * .11)
+        salaire = 25000
+        impot = (salaire * .9 - 10084) * .11
+        decote = 779 - impot * .4525
+
+        self.sim.set_input('salaire_imposable', annee, salaire)
+
+        self.assertAlmostEqual(self.sim.calculate('ir_ss_qf', annee)[0], impot, 1)
+        self.assertAlmostEqual(self.sim.calculate('decote', annee)[0], decote, 0)
+
+        self.assertAlmostEqual(self.sim.calculate('irpp', annee)[0], -round(impot) + decote, 1)
 
     def test02(self):
-        annee = '2020'
         self.sim.set_input('f4ba', annee, 10000)
         self.assertEqual(self.sim.calculate('revenu_categoriel_foncier', annee), [10000])
 
