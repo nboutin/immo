837d74fa50c1eff76ff557b4311dfa1d94747718
nboutin <boutwork@gmail.com>
2021-04-13 15:56:29 +0200

    Add Entity Fiscalite and Analyse

--- src/immo_analyse/model/entities.py
+++ src/immo_analyse/model/entities.py
@@ -12,5 +12,7 @@ from immo_analyse.core.entity import Entity
 Lot = Entity(key='lot')
 BienImmo = Entity(key='bien_immo', sub_entities=[Lot])
 Credit = Entity(key='credit')
+Fiscalite = Entity(key='fiscalite')
+Analyse = Entity(key='analyse')
 
-entities = [Lot, BienImmo, Credit]
+entities = [Lot, BienImmo, Credit, Fiscalite, Analyse]
